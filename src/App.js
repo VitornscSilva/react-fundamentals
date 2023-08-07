a382783eb2925b3daa45ac858ea1385e545176d0
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-08-06 23:51:27 -0300

    feat: add Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
-function App() {
+import Header from './Header';
+import Post from './Post';
+
+export default function App() {
   return (
     <>
-      <h1>App Component</h1>
-      <h2>Subtitle</h2>
+      <Header>
+        <h2>Posts of the week</h2>
+      </Header>
+
+      <hr />
+
+      <Post
+        likes={20}
+        post={{
+          title: "Title 1",
+          subtitle: "Subtitle 1",
+        }}
+      />
+      <Post
+        likes={50}
+        post={{
+          title: "Title 2",
+          subtitle: "Subtitle 2",
+        }}
+      />
+      <Post
+        likes={10}
+        post={{
+          title: "Title 3",
+          subtitle: "Subtitle 3",
+        }}
+      />
     </>
   );
 }
-
-export default App;
