92d1941d40898fde353b07847f4c74782a492faa
VitornscSilva <vitornscsilva.dev@gmail.com>
2023-08-13 17:17:55 -0300

    feat: configured Webpack to utilize SASS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Post from './Post';
 import { ThemeProvider } from './ThemeContext';
 
-import styles from './App.css';
+import styles from './App.scss';
 
 export default function App() {
   const [posts, setPosts] = useState([
